9eee0b1c776e79538658e9b284db497db8d24f1c
Quan2808 <nhotecfa@gmail.com>
2023-12-15 09:20:05 +0700

    v1

---
+++ src/WebApp/Areas/Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApp.Areas.Identity.Data;
+
+namespace WebApp.Areas.Identity.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        foreach (var entityType in builder.Model.GetEntityTypes())
+        {
+            var tableName = entityType.GetTableName();
+            if (tableName!.StartsWith("AspNet"))
+            {
+                entityType.SetTableName(tableName.Substring(6));
+            }
+        }
+
+        builder.Entity<ApplicationUser>()
+            .Property(u => u.PasswordHash)
+            .HasColumnName("Password");
+
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+
+    public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.Property(u => u.FirstName).HasMaxLength(50);
+            builder.Property(u => u.LastName).HasMaxLength(50);
+            builder.Property(u => u.Address).HasMaxLength(255);
+            builder.Property(u => u.Subscribe);
+        }
+    }
+}
