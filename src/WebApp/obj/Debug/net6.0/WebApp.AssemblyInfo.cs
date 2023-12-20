e14fe3244007d36480d373ed796fb37801653c3b
Quan2808 <nhotecfa@gmail.com>
2023-12-20 22:53:26 +0700

    new commit

--- src/WebApp/obj/Debug/net6.0/WebApp.AssemblyInfo.cs
+++ src/WebApp/obj/Debug/net6.0/WebApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1564b980eb1e1e7d4f4fa171abb2b5d2739a942")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bcdd0341209955b4d9d21fc447f814be659bc45")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
