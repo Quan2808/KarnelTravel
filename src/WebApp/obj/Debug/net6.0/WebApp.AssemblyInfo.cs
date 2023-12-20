a35a8543828f275075dc50dca059492bec8facd7
Quan2808 <nhotecfa@gmail.com>
2023-12-20 23:45:41 +0700

    Xóa rác

--- src/WebApp/obj/Debug/net6.0/WebApp.AssemblyInfo.cs
+++ src/WebApp/obj/Debug/net6.0/WebApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e14fe3244007d36480d373ed796fb37801653c3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b25d6bb3309c241906d115c6e4f32ed5358380dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
