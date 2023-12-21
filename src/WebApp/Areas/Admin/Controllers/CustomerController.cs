3eef5177648d2404533c75e3dae7d83d078a3bcd
Quan2808 <nhotecfa@gmail.com>
2023-12-21 14:07:07 +0700

    add change role

--- src/WebApp/Areas/Admin/Controllers/CustomerController.cs
+++ src/WebApp/Areas/Admin/Controllers/CustomerController.cs
@@ -1,13 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Data;
 
 namespace WebApp.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CustomerController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _dbContext;
+
+        public CustomerController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext dbContext)
         {
-            return View();
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await (from user in _userManager.Users
+                               join userRole in _dbContext.UserRoles on user.Id equals userRole.UserId
+                               join role in _roleManager.Roles on userRole.RoleId equals role.Id
+                               select new
+                               {
+                                   UserName = user.UserName,
+                                   UserId = user.Id,
+                                   Name = user.FirstName + " " + user.LastName,
+                                   Email = user.Email,
+                                   Phone = user.PhoneNumber,
+                                   Role = role.Name
+                               }).ToListAsync();
+            var availableRoles = await _roleManager.Roles.ToListAsync();
+
+            ViewBag.AvailableRoles = availableRoles;
+
+            return View(users);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangeRole(string userId, string selectedRole)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var currentRole = _userManager.GetRolesAsync(user).Result.FirstOrDefault();
+
+            await _userManager.RemoveFromRoleAsync(user, currentRole);
+
+            await _userManager.AddToRoleAsync(user, selectedRole);
+
+            return RedirectToAction("Index");
         }
     }
 }
--- src/WebApp/obj/Debug/net6.0/WebApp.AssemblyInfo.cs
+++ src/WebApp/obj/Debug/net6.0/WebApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76eefdd4fda6ddc0f0da522df1f3434a9f66f225")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b81ff8575d86fa92ce1fa73b6e8f9ae20ef06ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
