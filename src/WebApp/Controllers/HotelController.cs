64ba12e2a67a8b91ffaae8b9ac734a20c90b95bf
Quan2808 <nhotecfa@gmail.com>
2023-12-29 02:16:22 +0700

    demo index hotel

---
+++ src/WebApp/Controllers/HotelController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+
+namespace WebApp.Controllers
+{
+    public class HotelController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HotelController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Hotels.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var hotel = await _context.Hotels.FirstOrDefaultAsync(m => m.ID == id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            return View(hotel);
+        }
+    }
+}
