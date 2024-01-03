389df94b6f388d25ae1f84506d1caa08fcd7dc7f
anhua39 <116139878+anhua39@users.noreply.github.com>
2024-01-04 00:21:40 +0700

    Trang Home
    AnHua

--- src/WebApp/Controllers/HomeController.cs
+++ src/WebApp/Controllers/HomeController.cs
@@ -1,14 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Model;
+using WebApp.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(string? search)
+        {
+            // Order hotels by ID in descending order and take the top 10
+            var hotels = await _context.Hotels
+                .OrderByDescending(h => h.ID)
+                .Take(10)
+                .ToListAsync();
+
+            var hotelData = hotels.Select(hotel => new
+            {
+                Hotel = hotel,
+                NumRatings = _context.Ratings.Count(r => r.Booking.HotelID == hotel.ID),
+                TotalRatingValue = _context.Ratings
+                                    .Where(r => r.Booking.HotelID == hotel.ID)
+                                    .Sum(r => r.Value)
+            });
+
+            if (!String.IsNullOrEmpty(search))
+            {
+                hotelData = hotelData.Where(data => data.Hotel.Location!.Contains(search)).ToList();
+            }
+
+            return View(hotelData);
         }
 
         public IActionResult Introduction()
